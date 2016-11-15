245a519d8fef070638b8b731cf020f72fc58f3ef
Edward Lim <sgedwardlim@gmail.com>
2016-11-14 23:55:24 -0800

    Bug Fix: Lecture model not properlly capturing dupicate data

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,5 +1,6 @@
 class Lecture < ActiveRecord::Base
 	# Ensures that when adding data from CSV files, there are no duplication of data
-	validates_uniqueness_of :course_id, :course_name, :professor
+	# WRONG! validates_uniqueness_of :course_id, :course_name, :professor
+	validates_uniqueness_of :course_id, :scope => [:course_name, :professor]
 	has_many :comments, dependent: :destroy
 end
